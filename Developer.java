0807740646690def6a69d0fe79fa4ec23fec4265
olgaDCI <olga.babina@dci-student.org>
2024-09-12 10:50:49 +0200

    Added exception handling

--- Developer.java
+++ Developer.java
@@ -3,10 +3,17 @@ class Developer extends Employee {
     private int hoursWorked;
 
     public void setHourlyRate(double hourlyRate) {
+        if (hourlyRate < 0) {
+            throw new IllegalArgumentException("Hourly rate cannot be negative.");
+        }
         this.hourlyRate = hourlyRate;
     }
 
     public void setHoursWorked(int hoursWorked) {
+
+        if (hoursWorked < 0) {
+            throw new IllegalArgumentException("Hours worked cannot be negative.");
+        }
         this.hoursWorked = hoursWorked;
     }
 
--- Manager.java
+++ Manager.java
@@ -3,10 +3,16 @@ class Manager extends Employee {
     private double bonus;
 
     public void setBaseSalary(double baseSalary) {
+        if (baseSalary < 0) {
+            throw new IllegalArgumentException("Base salary cannot be negative.");
+        }
         this.baseSalary = baseSalary;
     }
 
     public void setBonus(double bonus) {
+        if (bonus < 0) {
+            throw new IllegalArgumentException("Bonus cannot be negative.");
+        }
         this.bonus = bonus;
     }
 
